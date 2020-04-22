ac606a711b95664c27e299e859ffbd87be782fb5
yanwenyy <18738390058>
2020-04-22 22:16:33 +0800

    数据概览,整体数据分析,归属数据,招生进度,各机构招生情况样式

--- js/view.js
+++ js/view.js
@@ -27,19 +27,23 @@ $(function () {
         }
         vWeek_s = date.getDay();
         var html_year = year + "/" + month + "/" + day;
-        var html_hour = hour+ ":" + minute+ ":" + second;
-        var html_week =vWeek[vWeek_s];
+        var html_hour = hour + ":" + minute + ":" + second;
+        var html_week = vWeek[vWeek_s];
 
         $(".time-year").html(html_year);
         $(".time-hour").html(html_hour);
         $(".time-week").html(html_week);
-    };
+    }
+
     setInterval(function () {
         time()
     }, 1000);
     totalData();
+    ascription();
+    student();
+
     //整体情况分析
-    function totalData(){
+    function totalData() {
         // 基于准备好的dom，初始化echarts实例
         var myChart = echarts.init(document.getElementById('totalData'));
         // 指定图表的配置项和数据
@@ -49,61 +53,382 @@ $(function () {
                 trigger: 'axis'
             },
             legend: {
-                data: ['2019', '2020']
+                data: ['2019年', '2020年'],
+                textStyle: {color: '#2BB5FF'}
             },
-
             xAxis: {
                 type: 'category',
                 boundaryGap: false,
+
                 data: [
                     {
-                        value:'周一',
-                        textStyle:{color:'#2BB5FF'}
+                        value: '周一',
+                        textStyle: {color: '#2BB5FF'}
                     }, {
-                        value:'周二',
-                        textStyle:{color:'#2BB5FF'}
+                        value: '周二',
+                        textStyle: {color: '#2BB5FF'}
                     }, {
-                        value:'周三',
-                        textStyle:{color:'#2BB5FF'}
+                        value: '周三',
+                        textStyle: {color: '#2BB5FF'}
                     }, {
-                        value:'周四',
-                        textStyle:{color:'#2BB5FF'}
+                        value: '周四',
+                        textStyle: {color: '#2BB5FF'}
                     }, {
-                        value:'周五',
-                        textStyle:{color:'#2BB5FF'}
+                        value: '周五',
+                        textStyle: {color: '#2BB5FF'}
                     }, {
-                        value:'周六',
-                        textStyle:{color:'#2BB5FF'}
-                    }, ],
-
-                name:'日期',
+                        value: '周六',
+                        textStyle: {color: '#2BB5FF'}
+                    },],
+                axisLine: {
+                    lineStyle: {
+                        color: '#2BB5FF',
+                    }
+                },
+                name: '日期',
                 nameTextStyle: {
-                    color:'#2BB5FF'
+                    color: '#2BB5FF'
                 }
             },
             yAxis: {
                 type: 'value',
-                name:'人数',
+                axisLine: {
+                    lineStyle: {
+                        color: '#2BB5FF',
+                    }
+                },
+                splitLine: {lineStyle: {color: '#2BB5FF'}},
+                name: '人数',
                 nameTextStyle: {
-                    color:'#2BB5FF'
+                    color: '#2BB5FF'
                 }
             },
             series: [
                 {
-                    name: '2019',
+                    name: '2019年',
                     type: 'line',
+                    symbolSize: 8,
                     data: [85, 70, 15, 87, 39, 67, 67],
-                    itemStyle : { normal: {label : {show: true}}}
+                    itemStyle: {normal: {label: {show: true}}},
+                    lineStyle: {color: '#FD375C'}
                 },
                 {
-                    name: '2020',
+                    name: '2020年',
                     type: 'line',
+                    symbolSize: 8,
                     data: [14, 45, 65, 12, 33, 23, 56],
-                    itemStyle : { normal: {label : {show: true}}}
+                    itemStyle: {normal: {label: {show: true, color: "#73D3E2"}}},
+                    lineStyle: {color: '#2B44A4'}
                 }
             ]
         };
+        myChart.setOption(option);
+    }
 
+    //归属数据
+    function ascription() {
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('ascription'));
+        // 指定图表的配置项和数据
+        var option = {
+            title: [{
+                text: ''
+            }, {
+                subtext: '全部',
+                subtextStyle: {
+                    color: '#2BB6FF'
+                },
+                left: '17%',
+                top: '37%',
+                textAlign: 'center'
+            }, {
+                subtext: '北京小泽',
+                subtextStyle: {
+                    color: '#2BB6FF'
+                },
+                left: '38%',
+                top: '37%',
+                textAlign: 'center'
+            }, {
+                subtext: '济南小泽',
+                subtextStyle: {
+                    color: '#2BB6FF'
+                },
+                left: '58%',
+                top: '37%',
+                textAlign: 'center'
+            },
+                {
+                    subtext: '郑州小泽',
+                    subtextStyle: {
+                        color: '#2BB6FF'
+                    },
+                    left: '78%',
+                    top: '37%',
+                    textAlign: 'center'
+                }, {
+                    subtext: '广州一尚',
+                    subtextStyle: {
+                        color: '#2BB6FF'
+                    },
+                    left: '18%',
+                    top: '82%',
+                    textAlign: 'center'
+                }, {
+                    subtext: '西安清美',
+                    subtextStyle: {
+                        color: '#2BB6FF'
+                    },
+                    left: '38%',
+                    top: '82%',
+                    textAlign: 'center'
+                },
+                {
+                    subtext: '西安青卓',
+                    subtextStyle: {
+                        color: '#2BB6FF'
+                    },
+                    left: '58%',
+                    top: '82%',
+                    textAlign: 'center'
+                },
+                {
+                    subtext: '北京九鼎',
+                    subtextStyle: {
+                        color: '#2BB6FF'
+                    },
+                    left: '78%',
+                    top: '82%',
+                    textAlign: 'center'
+                }],
+            color: ['#FF8A00', '#8D2EFB', '#FF0D4D', '#00DCD9', '#008EFF'],
+            tooltip: {},
+            dataset: {
+                source: [
+                    ['name', '全部', '北京小泽', '济南小泽', '郑州小泽', '广州一尚', '西安清美', '西安青卓', '北京九鼎'],
+                    ['线上', 41.1, 30.4, 65.1, 53.3, 83.8, 98.7, 73.4, 55.1],
+                    ['教学部', 86.5, 92.1, 85.7, 83.1, 73.4, 55.1, 83.8, 98.7],
+                    ['地推', 24.1, 67.2, 79.5, 86.4, 65.2, 82.5, 92.1, 85.7],
+                    ['画室', 55.2, 67.1, 69.2, 72.4, 53.9, 39.1, 86.4, 65.2],
+                    ['其他', 55.2, 67.1, 69.2, 72.4, 53.9, 39.1, 55.2, 67.1]
+                ]
+            },
+            series: [{
+                type: 'pie',
+                radius: 60,
+                center: ['18%', '20%'],
+                label: {
+                    position: 'inside',
+                    formatter: '{b}\n{d}%',
+                    fontSize: 10,
+                },
+                encode: {
+                    itemName: 'name',
+                    value: '全部'
+                }
+                // No encode specified, by default, it is '2012'.
+            }, {
+                type: 'pie',
+                radius: 60,
+                center: ['38%', '20%'],
+                label: {
+                    position: 'inside',
+                    formatter: '{b}\n{d}%',
+                    fontSize: 10,
+                },
+                encode: {
+                    itemName: 'name',
+                    value: '北京小泽'
+                }
+            }, {
+                type: 'pie',
+                radius: 60,
+                center: ['58%', '20%'],
+                label: {
+                    position: 'inside',
+                    formatter: '{b}\n{d}%',
+                    fontSize: 10,
+                },
+                encode: {
+                    itemName: 'name',
+                    value: '济南小泽'
+                }
+            },
+                {
+                    type: 'pie',
+                    radius: 60,
+                    center: ['78%', '20%'],
+                    label: {
+                        position: 'inside',
+                        formatter: '{b}\n{d}%',
+                        fontSize: 10,
+                    },
+                    encode: {
+                        itemName: 'name',
+                        value: '郑州小泽'
+                    }
+                },
+                {
+                    type: 'pie',
+                    radius: 60,
+                    center: ['18%', '65%'],
+                    label: {
+                        position: 'inside',
+                        formatter: '{b}\n{d}%',
+                        fontSize: 10,
+                    },
+                    encode: {
+                        itemName: 'name',
+                        value: '广州一尚'
+                    }
+                }, {
+                    type: 'pie',
+                    radius: 60,
+                    center: ['38%', '65%'],
+                    label: {
+                        position: 'inside',
+                        formatter: '{b}\n{d}%',
+                        fontSize: 10,
+                    },
+                    encode: {
+                        itemName: 'name',
+                        value: '西安清美'
+                    }
+                }, {
+                    type: 'pie',
+                    radius: 60,
+                    center: ['58%', '65%'],
+                    label: {
+                        position: 'inside',
+                        formatter: '{b}\n{d}%',
+                        fontSize: 10,
+                    },
+                    encode: {
+                        itemName: 'name',
+                        value: '西安青卓'
+                    }
+                },
+                {
+                    type: 'pie',
+                    radius: 60,
+                    center: ['78%', '65%'],
+                    label: {
+                        position: 'inside',
+                        formatter: '{b}\n{d}%',
+                        fontSize: 10,
+                    },
+                    encode: {
+                        itemName: 'name',
+                        value: '北京九鼎'
+                    }
+                }]
+        };
+        myChart.setOption(option);
+    }
+
+    //各机构招生情况
+    function student() {
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('student'));
+        // 指定图表的配置项和数据
+        var     option = {
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                    type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                }
+            },
+            legend: {
+                data: ['当天', '前一天', '前两天', '前三天', '前四天', '前五天', '前六天'],
+                bottom: '5%',
+                textStyle:{
+                    color:'#fff'
+                }
+            },
+            grid: {
+                left: '3%',
+                right: '4%',
+                bottom: '15%',
+                containLabel: true
+            },
+            xAxis: [
+                {
+                    type: 'category',
+                    data: ['北京小泽', '济南小泽', '郑州小泽', '广州一尚', '西安清美', '西安青卓', '北京九鼎', '画室'],
+                    axisLine: {
+                        lineStyle: {
+                            color: '#2BB5FF',
+                        }
+                    },
+                }
+            ],
+            yAxis: [
+                {
+                    type: 'value',
+                    splitLine: {lineStyle: {color: '#2BB5FF',type: 'dotted'}},
+                    axisLine: {
+                        lineStyle: {
+                            color: '#2BB5FF',
+                        },
+                    },
+                }
+            ],
+            series: [
+
+                {
+                    name: '当天',
+                    type: 'bar',
+                    barWidth: '25',
+                    stack: '广告',
+                    itemStyle: {normal: {color: '#19287A', label: {show: true}}},
+                    data: [120, 132, 101, 134, 90, 230, 210, 90]
+                },
+                {
+                    name: '前一天',
+                    type: 'bar',
+                    stack: '广告',
+                    itemStyle: {normal: {color: '#39D9D6', label: {show: true}}},
+                    data: [220, 182, 191, 234, 290, 330, 310, 139]
+                },
+                {
+                    name: '前两天',
+                    type: 'bar',
+                    stack: '广告',
+                    itemStyle: {normal: {color: '#8433FD', label: {show: true}}},
+                    data: [150, 232, 201, 154, 190, 330, 410, 120]
+                },
+                {
+                    name: '前三天',
+                    type: 'bar',
+                    barWidth: '25',
+                    stack: '广告',
+                    itemStyle: {normal: {color: '#E8C212', label: {show: true}}},
+                    data: [120, 132, 101, 134, 90, 230, 210, 90]
+                },
+                {
+                    name: '前四天',
+                    type: 'bar',
+                    stack: '广告',
+                    itemStyle: {normal: {color: '#3ECF67', label: {show: true}}},
+                    data: [220, 182, 191, 234, 290, 330, 310, 120]
+                },
+                {
+                    name: '前五天',
+                    type: 'bar',
+                    stack: '广告',
+                    itemStyle: {normal: {color: '#FD375C', label: {show: true}}},
+                    data: [150, 232, 201, 154, 190, 330, 410, 340]
+                }, {
+                    name: '前六天',
+                    type: 'bar',
+                    stack: '广告',
+                    itemStyle: {normal: {color: '#FF9019', label: {show: true}}},
+                    data: [150, 232, 201, 154, 190, 330, 410, 200]
+                },
+
+
+            ]
+        };
         myChart.setOption(option);
     }
 });
